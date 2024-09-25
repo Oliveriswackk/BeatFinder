e444759fce8925f998dcaaed35c6a07f405a0cc1
Oliver Coronado <95058229+Oliveriswackk@users.noreply.github.com>
2024-09-25 07:07:52 -0600

    coco

--- beatfinder/src/App.js
+++ beatfinder/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chambing in proccess :<code>(</code>
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ¡¡Darse de Baja!! 🥳
         </a>
       </header>
     </div>
