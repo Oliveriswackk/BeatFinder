68fa479c310bd430d058884d7ab5e4bbc29c8863
Oliver Coronado <95058229+Oliveriswackk@users.noreply.github.com>
2024-09-26 18:54:20 -0600

    Creación de Home y subir imagenes del proyecto

--- beatfinder/src/App.js
+++ beatfinder/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Componentes/Home';
 
 function App() {
   return (
     
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chambing in proccess :<code>(</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ¡¡Darse de Baja!! 🥳
-        </a>
+       
+        <Home />
       </header>
     </div>
   );
