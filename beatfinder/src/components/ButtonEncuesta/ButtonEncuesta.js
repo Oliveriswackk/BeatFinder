7a6ea63f99d7621ce7da58a0d8817cdeec1943e5
Oliver Coronado <95058229+Oliveriswackk@users.noreply.github.com>
2024-10-09 10:44:19 -0600

    Ajustes header y boton y fondos

--- beatfinder/src/components/ButtonEncuesta/ButtonEncuesta.js
+++ beatfinder/src/components/ButtonEncuesta/ButtonEncuesta.js
@@ -1,9 +1,9 @@
+// src/components/ButtonEncuesta/ButtonEncuesta.js
 import React from 'react';
 import './ButtonEncuesta.css';
 
 const ButtonEncuesta = () => {
   const handleClick = () => {
-    // Lógica para manejar el clic en el botón de encuesta
     alert('¡Encuesta abierta!');
   };
 
