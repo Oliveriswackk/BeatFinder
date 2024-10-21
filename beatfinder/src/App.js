8c0321fa436e352d6cc13d2114a4e6750a5a8885
Jorge Chavira <jorgechavira4@gmail.com>
2024-10-21 11:29:00 -0600

    Cambio de enrutamientos y simplificacion del codigo en general

--- beatfinder/src/App.js
+++ beatfinder/src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Componentes/Home';
 import Artistas_Destacados from './Componentes/Artistas_Destacados';
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Encuesta from './Componentes/Encuesta';
+import './App.css';
 
 function App() {
   return (
-    
     <div className="App">
-      <header className="App-header">
-       
-        <Home />
-      </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/encuesta" element={<Encuesta />} />
+          {/* Add more routes as needed */}
+        </Routes>
     </div>
   );
 }
