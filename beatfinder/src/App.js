118fd20fe8c521f38a710e980ec11c4451f74445
Jorge Chavira <jorgechavira4@gmail.com>
2024-10-25 18:34:25 -0600

    Eliminacion de componentes no utilizados.

--- beatfinder/src/App.js
+++ beatfinder/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import Encuesta from './Componentes/Encuesta';
 import './App.css';
 import Home from './Componentes/Home';
-import Artistas_Destacados from './Componentes/Artistas_Destacados';
+
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
