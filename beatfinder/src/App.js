cdc9ae8701e812c39abcd830a10bce83f4c5eeed
Oliver Coronado <95058229+Oliveriswackk@users.noreply.github.com>
2024-10-26 12:58:42 -0600

    Acomodo pt.666

--- beatfinder/src/App.js
+++ beatfinder/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import Encuesta from './Componentes/Encuesta';
+import Encuesta from './routes/Encuesta';
 import './App.css';
-import Home from './Componentes/Home';
+import Home from './routes/Home';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
