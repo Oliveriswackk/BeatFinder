2d4703c24a2b00d0421bc51fb51eef4b675ae998
Oliver Coronado <95058229+Oliveriswackk@users.noreply.github.com>
2024-11-05 08:00:17 -0600

    Creacion componentes top 69

---
+++ beatfinder/src/Top69/components/top.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/BackgroundImage.css';
+
+const TopImage = () => {
+  return (
+    <div className='container'>
+        <div className='image'>
+            <img src={fondo} alt="" />
+        </div>
+      
+    </div>
+  );
+};
+
+export default BackgroundImage;
