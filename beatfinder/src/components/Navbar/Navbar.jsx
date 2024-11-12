d65c082de6261e2458adcc25a0a5a02841d4e300
Jorge Chavira <jorgechavira4@gmail.com>
2024-11-11 20:25:32 -0600

    Modificacion de como se muestran las rutas en la NavBar

--- beatfinder/src/components/Navbar/Navbar.jsx
+++ beatfinder/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,86 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import '../Navbar/Navbar.css';
+import logo from '../../images/BF_Logo.png';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <a href="/radio-spin">Radio Spin</a>
-    <a href="/">BeatFinder</a>
-    <a href="/artistas-destacados">Artista Destacado</a>
-  </nav>
-);
+function Navbar() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  if (currentPath === '/top69') {
+    
+    return (
+      <nav className="navbar">
+        <a href="/artistas-destacados">Artista Destacado</a>
+        {logo ? (
+            <a href="/">
+              <img src={logo} alt="BeatFinder Logo" className="navbar-logo" />
+            </a>
+        ):(
+            <a href="/">
+              BeatFinder
+            </a>
+        )}
+        
+        <a href="/radio-spin">Radio Spin</a>
+      </nav>
+    );
+  }
+  if (currentPath === '/radio-spin') {
+    
+    return (
+      <nav className="navbar">
+        <a href="/artistas-destacados">Artista Destacado</a>  
+        {logo ? (
+          <a href="/">
+            <img src={logo} alt="BeatFinder Logo" className="navbar-logo" />
+          </a>
+        ):(
+          <a href="/">
+            BeatFinder
+          </a>
+        )}
+        
+        <a href="/top69">top69</a>
+      </nav>
+    );
+  }
+  if (currentPath === '/artistas-destacados') {
+    
+    return (
+      <nav className="navbar">
+        <a href="/radio-spin">Radio Spin</a>
+        {logo ? (
+            <a href="/">
+              <img src={logo} alt="BeatFinder Logo" className="navbar-logo" />
+            </a>
+        ):(
+            <a href="/">
+              BeatFinder
+            </a>
+        )}
+        <a href="/top69">top69</a>
+      </nav>
+    );
+  }
+  if (currentPath === '/encuesta') {
+    
+    return (
+      <nav className="navbar">
+                <a href="/radio-spin">Radio Spin</a>
+        {logo ? (
+            <a href="/">
+              <img src={logo} alt="BeatFinder Logo" className="navbar-logo" />
+            </a>
+        ):(
+            <a href="/">
+              BeatFinder
+            </a>
+        )}
+        
+        <a href="/top69">top69</a>
+      </nav>
+    );
+  }
+}
 
 export default Navbar;
