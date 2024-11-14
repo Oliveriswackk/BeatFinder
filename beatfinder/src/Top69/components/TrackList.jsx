62265b803a7d7fd780f3280a01ee09fbc6b309e7
Jorge Chavira <jorgechavira4@gmail.com>
2024-11-14 12:42:25 -0600

    Modificacion de API. Se utiliza la api de spotify. Tambien se instalaron el: - Axios - Buffer para el uso de la api

--- beatfinder/src/Top69/components/TrackList.jsx
+++ beatfinder/src/Top69/components/TrackList.jsx
@@ -3,30 +3,66 @@ import TrackItem from './TrackItem';
 import FeaturedTrack from '../../Top69/components/FeaturedTrack';
 import '../styles/TrackList.css';
 
+import axios from 'axios';
+import { Buffer } from 'buffer';
+
+const client_id = 'd52221bf672f4fa4849370f393f65ad5';
+const client_secret = 'a65efe8d97884d7f830aab11ddc8f839';
+
 function TrackList() {
   const [tracks, setTracks] = useState([]);
   const [selectedTrack, setSelectedTrack] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState(null);
+
+  const getSpotifyToken = async () => {
+    if (token) return;
+
+    try {
+      const authUrl = 'https://accounts.spotify.com/api/token';
+      const authString = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+
+      const response = await axios.post(
+        authUrl,
+        new URLSearchParams({ grant_type: 'client_credentials' }),
+        {
+          headers: {
+            Authorization: `Basic ${authString}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      );
+      setToken(response.data.access_token);
+    } catch (error) {
+      console.error('Error fetching Spotify token:', error);
+    }
+  };
 
   const fetchTracks = async () => {
+    if (!token) return;
     setLoading(true);
 
     try {
-      const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/tracks?limit=69');
-      const data = await response.json();
-      console.log('API Response:', data);
+      const response = await axios.get(
+        'https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M/tracks?limit=50',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-      if (data.data) {
-        const formattedTracks = data.data.map(track => ({
-          id: track.id,
-          title: track.title,
-          artist: track.artist.name,
-          albumArtUrl: track.album.cover_big
+      if (response.data.items) {
+        const formattedTracks = response.data.items.map(item => ({
+          id: item.track.id,
+          title: item.track.name,
+          artist: item.track.artists[0].name,
+          albumArtUrl: item.track.album.images[0].url,
         }));
         setTracks(formattedTracks);
         setSelectedTrack(formattedTracks[0]);
       } else {
-        console.error('Unexpected data format:', data);
+        console.error('Unexpected data format:', response.data);
       }
     } catch (error) {
       console.error('Error fetching tracks:', error);
@@ -35,20 +71,16 @@ function TrackList() {
     }
   };
 
-  useEffect(() => {
-    fetchTracks();
-  }, []);
+  useEffect(() => { getSpotifyToken().then(fetchTracks);}, [token]);
 
   return (
     <div className="tracklist-container">
       <div className="featured-track-container">
-        {selectedTrack && (
-          <FeaturedTrack track={selectedTrack} />
-        )}
+        {selectedTrack && <FeaturedTrack track={selectedTrack} />}
       </div>
       <div className="tracklist">
         {loading ? (
-          <p className="text-white">Cargando...</p>
+          <p className="text-white">Loading...</p>
         ) : (
           tracks.map((track, index) => (
             <TrackItem
