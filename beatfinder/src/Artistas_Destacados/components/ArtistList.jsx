0145173494e64c8d285a97ab449f6ac3ca6e789c
Jorge Chavira <jorgechavira4@gmail.com>
2024-11-20 19:50:22 -0600

    Se creo una lista para almacenar a otros artistas destacados

---
+++ beatfinder/src/Artistas_Destacados/components/ArtistList.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/ArtistList.css';
+const ArtisList = ({ artists, onArtistClick }) => {
+    return (
+      <div className="artists-list-container">
+        <h2>Más Artistas</h2>
+        <div className="artists-list">
+          {artists.map((artist, index) => (
+            <div 
+              key={index} 
+              className="artist-list-item" 
+              onClick={() => onArtistClick(artist)}
+            >
+              <div className="artist-art-container">
+                <img
+                  src={artist.imagen || '/placeholder.png'}
+                  alt={artist.nombre}
+                  className="artist-art"
+                />
+              </div>
+              <div className="artist-info">
+                <h3 className="artist-name">{artist.nombre}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+export default ArtisList;
